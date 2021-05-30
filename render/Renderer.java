54009900e0c10ccf818066abffccfb4fc39859dc
aa-ryan <aryansinha6363@gmail.com>
2021-05-30 12:28:11 +0530

    Render and Jframe issue fixed

---
+++ render/Renderer.java
@@ -0,0 +1,8 @@
+package render;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel {
+	public static final long serialVersionUID = 1L;
+
+}
--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,5 +1,26 @@
-package flappyBird;
+package src.flappyBird;
+
+import javax.swing.JFrame;
 
 public class FlappyBird {
-	
+
+	public static FlappyBird flappyBird;   // static instance of FlappyBird
+
+	public final int WIDTH = 800, HEIGHT = 800;
+
+	public FlappyBird() {
+
+		JFrame jframe = new JFrame();
+
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(WIDTH, HEIGHT);
+		jframe.setResizable(false);
+		jframe.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		flappyBird = new FlappyBird();
+	}
+
 }
