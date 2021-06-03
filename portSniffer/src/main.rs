343c205a7a264d914dfeff92309fd2932db3cdba
aa-ryan <aryansinha6363@gmail.com>
2021-06-03 07:54:04 +0530

    Added Basic Structures

---
+++ portSniffer/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let _program = args[0].clone();
+
+}
